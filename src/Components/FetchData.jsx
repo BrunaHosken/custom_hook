c2cb75b830ee5f4d80bf48b546702932203e7023
Bruna Hosken <bruna16.hosken@gmail.com>
2025-11-29 18:27:13 -0500

    criando aplicativo fetch

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
+import UseFetch from './UseFetch'
+import fruitData from './fruit.json';
+import './FetchData.css';
 
 const FetchData = () => {
-  return (
-    <>
-     <ul className='list_data_main'>
-        <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
-     </ul>
-    </>
-  )
+
+    // const [data] = UseFetch('https://api.npoint.io/9045c260b1565daa9e15');
+    // console.log(data)
+    const data = fruitData;
+
+    return (
+        <>
+            <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
+            <ul className='list_data_main'>
+                {data && data.map((e,index)=>(
+                    <li key={index} className='list_data'>
+                        <h3>{e.name}</h3>
+                        <img src={e.image} alt={e.name} width="200" />
+                        <p><strong>Importance: </strong>{e.importance}</p>
+                        <p><strong>Benefits: </strong>{e.benefits}</p>
+                        <p><strong>Time to eat: </strong>{e.best_time_to_intake}</p>
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
 }
 
 export default FetchData
